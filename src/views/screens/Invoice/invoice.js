901d1c531377310f0ebe5de17ec3555145626cc0
Dixith Maram <dixithmaram@Dixiths-MacBook-Air.local>
2021-05-03 01:53:24 -0500

    add selected item

--- src/views/screens/Invoice/invoice.js
+++ src/views/screens/Invoice/invoice.js
@@ -6,62 +6,91 @@ import instance from '../../../utils/Networking/network'
 //mport { faHome } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Select from 'react-select'
+import ReactPaginate from 'react-paginate'
 
 class Invoice extends Component{
     constructor(props){
-        super(props)
-        this.state = {
-           items: [],
-           rows: [{
-            desc: "Hello",
-            quantity: 1,
-            price: 1,
-            amount: 1
-           },
-           {
-            desc: "Hello",
-            quantity: 1,
-            price: 1,
-            amount: 1
-           },
-           {
-            desc: "Hello",
-            quantity: 1,
-            price: 1,
-            amount: 1
-           },
-           {
-            desc: "Hello",
-            quantity: 1,
-            price: 1,
-            amount: 1
-           }],
-            options: [
-            { value: 'chocolate', label: 'Chocolate' },
-            { value: 'strawberry', label: 'Strawberry' },
-            { value: 'vanilla', label: 'Vanilla' }
-          ]
+      super(props)
+      this.state = {
+      items: [],
+      rows: {"index":1, value: {}},
+      offset: 0,
+      data: [],
+      perPage: 1,
+      currentPage: 0,
+      options: [],
+      selectedId: "",
+      selectedValue: "",
+      selectedDescription: "",
+      selectedQty: 0,
+      price: "",
+      selectedPrice: "",
+      amount: "",
+      addedItems: []
         }
-        // this.addRow = this.addRow.bind(this)
         this.deleteRow = this.deleteRow.bind(this)
         this.getLineItem = this.getLineItem.bind(this)
-    }
-
-    componentDidMount(){
-        this.getLineItem()
+      
     }
 
     addRow = () => {
-        this.setState((prevState, props) => {
-          const row =  {
-            desc: "Hello",
-            quantity: 1,
-            price: 1,
-            amount: 1
-        };
-          return { rows: [...prevState.rows, row] };
-        });
-      };
+        const {options, selectedId, rows,selectedValue, selectedQty, selectedPrice, amount, price} = this.state;
+        console.log(selectedValue, selectedQty, price, amount, "--needed details are here--")
+       var selectedItm = options.forEach(option => {
+            console.log(option, "--options is here--")
+            if(option.label == selectedValue){
+                return option.value
+            }
+            this.setState(prevState => (
+                console.log("--check how many times this calls--"),
+                { 
+                rows: {
+                "index": this.state.rows.index + 1, 
+                "value":  selectedItm
+        },
+            price: "",
+                addedItems: [...prevState.addedItems, 
+                    {
+                        "id": selectedId,
+                        "item": this.state.selectedValue,
+                        "details":"this is a regular demo item",
+                        "quantity": selectedQty,
+                        "price": selectedPrice,
+                        "amount": selectedQty * price,
+                    },
+                ]
+            }))
+        })
+        // this.setState(prevState => (
+        //     console.log("--check how many times this calls--"),
+        //     { 
+        //     rows: {
+        //     "index": this.state.rows.index + 1, 
+        //     "value":  
+        //     {selectedQty: 0,
+        //     selectedPrice: "",
+        //     price: price,
+        //     amount: ""
+        // }},
+        // price: "",
+        //     addedItems: [...prevState.addedItems, 
+        //         {
+        //             "id": selectedId,
+        //             "item": this.state.selectedValue,
+        //             "details":"this is a regular demo item",
+        //             "quantity": selectedQty,
+        //             "price": selectedPrice,
+        //             "amount": selectedQty * price,
+        //         },
+        //     ]
+        // })
+        
+        // )
+
+    //   }else{
+    //       console.log("please fill all the details")
+    //   }
+    }
     
       deleteRow = () => {
         this.setState((prevState, props) => {
@@ -69,22 +98,53 @@ class Invoice extends Component{
         });
       };
 
+      componentDidMount(){
+        this.getLineItem()
+      }
 
-      getLineItem = () => {
-        //   const {items} = this.state.items;
+      changeQty = (e)=> {
+        this.setState({
+            selectedQty: e.target.value,
+            amount: e.target.value * this.state.selectedPrice
+        })
+      }
+
+
+      selectedItem = (e) => {
+          this.setState({
+              selectedId: e.value.id,
+              selectedValue: e.label,
+              selectedDescription: e.value.details,
+              price: e.value.price
+          })
+    }
+
+      getLineItem = (props) => { 
         instance.get("/item").then(res => {
-            console.log(res,"---response is here---")
-            this.state.items = res.data.data
-            console.log(this.state.items,"---items are here---")
+            console.log(res.data.data, res.data.data.item,"--needs are here--")
+            this.setState({
+                items: res.data.data,
+                currentPage: res.data.current_page,
+                perPage: res.data.per_page,
+            })
+            this.state.items.forEach(item => {
+                this.setState(prevState => ({
+                    options: [...prevState.options, {value: item, label: item.item,
+                    }]
+                })) 
+            })
         }).catch(e => {
             console.log(e,"---error is here---")
         })
       }
+
     
 render(){
+    const {rows, options, addedItems, selectedQty, selectedPrice, price, amount} = this.state
+   console.log(addedItems[0],amount,"how many times render calls")
+ 
     return(
          <div className="invoiceSection">
-         
          <div>
              <span>
                  <input />
@@ -107,28 +167,49 @@ render(){
                       </tr>
                   </thead>
                   <tbody style={{border: "1px solid #eee", padding: "8px"}}>
-                  {this.state.rows.map(( listValue, index ) => {
+               {[...Array(this.state.rows.index)].map((row, i) =>  {
+                   console.log(this.state.rows,"--index is here--")
           return ( <tr>
-                      <td><Select options={this.state.options} /></td>
-                      <td><input /></td>
-                      <td><input /></td>
-                      <td>{listValue.amount}</td>
                       <td>
+                          <Select options={this.state.options}
+                          onChange={this.selectedItem}></Select>
+                          </td>
+                      <td><input id="qty" onChange={this.changeQty} /></td>
+
+                      <td>
+                        <label>{price || rows.value.price}</label> 
+                          </td>
+                      <td>
+                      {price  && <input value={(selectedQty * price)} />}
+                         </td>
+                      <td>
+
                           <FontAwesomeIcon  />
                           </td>
                       </tr> )})}
                   </tbody>
               </table>
               <button onClick={this.addRow}>Add Item</button>
+            </div>
           </div>
-         {/* <Footer /> */}
-        </div>
-    )
-}
-
-
-
+    )}
 
 }
 
-export default Invoice
+export default Invoice;
+
+   {/* {[...Array(this.state.options)].map((e, i) =>  {
+          return ( <tr>
+                      <td>
+                          <Select options={this.state.options} onChange={this.selectedItem}><option>Hello</option></Select>
+                          {/* {this.state.selectedItems.forEach((option, i) => <label> {
+                              option.meta.lineItems.item
+                          }</label>)} 
+                          </td>
+                      <td><input id="qty" /></td>
+                      <td></td>
+                      <td><input  /></td>
+                      <td>
+                          <FontAwesomeIcon  />
+                          </td>
+                      </tr> )})} */}
